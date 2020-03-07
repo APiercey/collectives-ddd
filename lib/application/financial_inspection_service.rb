15735253877b342c5f91aca926b9b5d2aa95f5a5
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-03-07 19:50:44 +0000

    Financial inspection service

---
+++ lib/application/financial_inspection_service.rb
@@ -0,0 +1,16 @@
+class FinancialInspectionService
+  def initialize(collective_repo)
+    @collective_repo = collective_repo
+  end
+
+  def calculate_total_assets
+    collective_repo.all
+      .group_by(&:currency)
+      .map { |currency, collectives| { currency => collectives.sum(&:balance) } }
+      .reduce({}, :merge)
+  end
+
+  private
+
+  attr_reader :collective_repo
+end
