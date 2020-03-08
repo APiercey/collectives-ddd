4efd33c137bf5a3bdcf83b566a3c0717b51b9350
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-03-08 22:01:47 +0000

    Run integration tests specificallt

--- spec/integration/domain/collective_repo_implementation_spec.rb
+++ spec/integration/domain/collective_repo_implementation_spec.rb
@@ -3,7 +3,7 @@
 require './lib/domain/collectives/collective_repo.rb'
 require_relative './collective_repo_shared_examples.rb'
 
-RSpec.describe Collectives::CollectiveRepo do
+RSpec.describe Collectives::CollectiveRepo, :integration do
   let(:client) { OpenCollective::Client.new }
 
   include_examples 'a collective repo' do
