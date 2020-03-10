36b1eb6720065dee0db22ea9255818ae9e4d1026
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-03-10 20:10:48 +0000

    Interface tests

--- spec/helpers/specification_behaviour.rb
+++ spec/helpers/specification_behaviour.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a specification' do
+  it { is_expected.to respond_to(:and).with(1).argument }
+  it { is_expected.to respond_to(:or).with(1).argument }
+  it { is_expected.to respond_to(:not).with(1).argument }
+  it { is_expected.to respond_to(:satisfies?).with(1).argument }
+
   describe '#satisifes?' do
-    describe 'canidate satisfies' do
+    describe 'valid canidate satisfies' do
       subject { spec.satisfies? valid_canidate }
 
       it { is_expected.to be_truthy }
     end
 
-    describe 'canidate does not satisfy' do
+    describe 'invalid canidate does not satisfy' do
       subject { spec.satisfies? invalid_candidate }
 
       it { is_expected.to be_falsy }
