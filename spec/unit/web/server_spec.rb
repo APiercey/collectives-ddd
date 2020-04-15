5cfcea9a2f014e5bbf5c0237883d7b062c2636c8
Alexander Butt-Piercey <alexander.piercey@gmail.com>
2020-04-15 18:35:03 +0000

    Refactor tests

--- spec/unit/web/server_spec.rb
+++ spec/unit/web/server_spec.rb
@@ -4,6 +4,7 @@ require 'rack/test'
 require './spec/helpers/test_server.rb'
 require './spec/helpers/test_application.rb'
 require './lib/infrastructure/exceptions.rb'
+require './spec/helpers/shared_examples/a_get_endpoint.rb'
 
 RSpec.describe TestServer do
   include Rack::Test::Methods
@@ -15,84 +16,52 @@ RSpec.describe TestServer do
   let(:application) { TestApplication.new }
   let(:sample_collective) { application.collective_repo.all.sample }
 
-  shared_examples 'a get endpoint' do
-    it 'returns status 200' do
-      expect(result.status).to eq 200
-    end
-
-    it 'returns Content-Type as application/json' do
-      header = result.headers['Content-Type']
-
-      expect(header).to be_truthy.and eql('application/json')
-    end
-  end
-
-  shared_examples 'object matches collective schema' do
-    it 'returns a matching collective' do
-      expect(result_collective).not_to be_nil
-
-      expect(result_collective).to include(
-        'contributors_count' => collective.contributors_count,
-        'slug' => collective.slug,
-        'image' => collective.image,
-        'backers_count' => collective.backers_count
-      )
-    end
+  let(:collective_schema) do
+    {
+      'contributors_count' => a_value,
+      'slug' => a_kind_of(String),
+      'image' => a_kind_of(String),
+      'backers_count' => a_value
+    }
   end
 
   describe 'GET /collectives' do
-    subject { get '/collectives' }
+    before { get '/collectives' }
 
-    it_behaves_like 'a get endpoint' do
-      let(:result) { subject }
-    end
+    it_behaves_like 'a get endpoint'
 
     describe 'body' do
-      let(:result) { JSON.parse(subject.body) }
+      subject { parsed_body }
 
-      it_behaves_like 'object matches collective schema' do
-        let(:result_collective) do
-          result.find { |c| c['id'] == collective.uuid }
-        end
-
-        let(:collective) { sample_collective }
-      end
-
-      it 'returns a collection' do
-        expect(result).to be_an Enumerable
-      end
+      it { is_expected.to be_an Enumerable }
+      it { is_expected.to all(match_schema(collective_schema)) }
     end
   end
 
   describe 'GET /collectives/:id' do
-    subject { get "/collectives/#{sample_collective.uuid}" }
+    before { get "/collectives/#{sample_collective.uuid}" }
 
-    it_behaves_like 'a get endpoint' do
-      let(:result) { subject }
-    end
+    it_behaves_like 'a get endpoint'
 
     describe 'body' do
-      it_behaves_like 'object matches collective schema' do
-        let(:result_collective) { JSON.parse(subject.body) }
-        let(:collective) { sample_collective }
-      end
+      subject { parsed_body }
+
+      it { is_expected.to match_schema(collective_schema) }
     end
   end
 
   describe 'GET /assets' do
-    subject { get '/assets' }
+    before { get '/assets' }
 
-    it_behaves_like 'a get endpoint' do
-      let(:result) { subject }
-    end
+    it_behaves_like 'a get endpoint'
 
     describe 'body' do
-      let(:result) { JSON.parse(subject.body) }
+      subject { parsed_body }
+
+      it { is_expected.not_to be_nil }
 
       it 'returns a list of assets' do
-        expect(result).not_to be_nil
-
-        expect(result).to include(
+        is_expected.to include(
           'USD' => 2000,
           'PLN' => 9000,
           'EUR' => 3800
@@ -103,38 +72,35 @@ RSpec.describe TestServer do
 
   context 'when an error occurs' do
     describe 'Exceptions::InternalError' do
-      let(:result) { get '/internal_error_route' }
+      subject { last_response }
 
-      it 'it returns status 422' do
-        expect(result.status).to eq 422
-      end
+      before { get '/internal_error_route' }
 
-      it 'returns Content-Type as application/json' do
-        header = result.headers['Content-Type']
+      it { is_expected.to have_status 422 }
+      it { should have_json_header }
 
-        expect(header).to be_truthy.and eql('application/json')
-      end
+      describe 'body' do
+        subject { parsed_body }
 
-      it 'provides a body' do
-        expect(JSON.parse(result.body))
-          .to include('code' => 422, 'message' => a_kind_of(String))
+        it do
+          is_expected.to include('code' => 422, 'message' => a_kind_of(String))
+        end
       end
     end
-  end
 
-  describe 'GET /collectives/:id/assets' do
-    subject { get "/collectives/#{sample_collective.uuid}/assets" }
+    describe 'GET /collectives/:id/assets' do
+      before { get "/collectives/#{sample_collective.uuid}/assets" }
 
-    it_behaves_like 'a get endpoint' do
-      let(:result) { subject }
-    end
+      it_behaves_like 'a get endpoint'
 
-    describe 'body' do
-      let(:result) { JSON.parse(subject.body) }
+      describe 'body' do
+        subject { parsed_body }
 
-      it 'returns the correct assets' do
-        report = sample_collective.financial_report
-        expect(result).to include(report.currency => report.balance)
+        let(:report) { sample_collective.financial_report }
+
+        it 'returns the correct assets' do
+          is_expected.to include(report.currency => report.balance)
+        end
       end
     end
   end
